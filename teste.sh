036d60be03f25ec5c1fc21acb9cf0bf927ced785
rodrigospedo <44686053+rodrigospedo@users.noreply.github.com>
2018-11-01 20:09:37 -0300

    Create teste.sh

---
+++ teste.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Meu Primeiro Script
+
+#listando os diretórios do linux
+ls -lha /
+read 
+clear
+
+#atualizando o apt ou apt-get
+apt update
+echo "Listas atualizadas!!!"
+read
+cls
+
+#desligando o servidor
+shutdown -h now

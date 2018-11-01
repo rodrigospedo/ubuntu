10794a2dde0c7bd4fad941e51cd3b965b596af0b
rodrigospedo <44686053+rodrigospedo@users.noreply.github.com>
2018-11-01 20:41:01 -0300

    Create rodrigo.sh

---
+++ rodrigo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Meu Segundo Script
+
+#atualização
+sudo apt update
+read
+sudo apt upgrade
+read
+echo "tudo atualizado"
+clear
+
+#baixando vim
+sudo apt install linuxlogo
+read
+clear
+
+#criar uma pasta no home
+mkdir -v ~/teste_rodrigo
+read
+clear
+
+#data e hora no linux
+date
+
+#reiniciando o servidor
+shutdown -r now
